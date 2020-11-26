fbbc80cc13ed3d7a854eaec4a5b92506f9b71f5b
Liberbell <liberbell@gmail.com>
2020-11-27 08:46:52 +0900

    getwithparam6

--- getwithparam.py
+++ getwithparam.py
@@ -5,4 +5,6 @@ url = "https://www.wikipedia.org"
 resp_obj = requests.get(url)
 
 print(resp_obj.url)
-webbrowser.open(resp_obj.url)
+# webbrowser.open(resp_obj.url)
+
+search_term = input("Enter the term you need to search: ")
