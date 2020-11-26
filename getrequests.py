b5e145a08bef48ecbda2ac055f4c9336ea9d1546
Liberbell <liberbell@gmail.com>
2020-11-27 08:38:25 +0900

    getrequests2

--- getrequests.py
+++ getrequests.py
@@ -6,17 +6,17 @@ print(requests.__version__)
 print(requests.__copyright__)
 
 url1 = "https://www.metaweather.com/api/location/2487956/2020/11/24" 
-r_get = requests.get(url1)
-print(r_get.status_code)
-print(type(r_get))
-print(type(r_get.headers))
+# r_get = requests.get(url1)
+# print(r_get.status_code)
+# print(type(r_get))
+# print(type(r_get.headers))
 
-pprint(r_get.headers)
+# pprint(r_get.headers)
 
-data = json.loads(r_get.text)
-pprint(data)
+# data = json.loads(r_get.text)
+# pprint(data)
 
-print(r_get.is_redirect)
+# print(r_get.is_redirect)
 
 r_get = requests.get("https://swapi.dev/api/planets/3")
 pprint(r_get.text)
