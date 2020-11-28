21a2b6fcfb07a89a8da89407257a177ed2964a5f
Liberbell <liberbell@gmail.com>
2020-11-28 14:02:42 +0900

    postrequests2

--- postrequests.py
+++ postrequests.py
@@ -2,3 +2,6 @@ import requests
 from pprint import pprint
 
 r_post = requests.post("https://en.wikipedia.org/w/index.php", data= {'search' : 'skillsoft'})
+
+print(r_post.status_code)
+print(type(r_post))
