ada8a1eff6927ed3f47d0e7b10726d24e22e069a
Liberbell <liberbell@gmail.com>
2020-11-28 14:16:39 +0900

    postrequests8

--- postrequests.py
+++ postrequests.py
@@ -1,14 +1,21 @@
 import requests
 from pprint import pprint
 
-r_post = requests.post("https://en.wikipedia.org/w/index.php", data= {'search' : 'skillsoft'})
+r_post = requests.post("https://en.wikipedia.org/w/index.php", data = {'search' : 'skillsoft'})
 
+# print(r_post.status_code)
+# print(type(r_post))
+
+# pprint(r_post.text)
+# print(r_post.url)
+
+# with open("skillsoft.html", "wb") as f:
+#     for chunk in r_post.iter_content(chunk_size=10000):
+#         f.write(chunk)
+
+r_post = requests.post("https://httpbin.org/#/HTTP_Methods/post_post")
 print(r_post.status_code)
 print(type(r_post))
 
 pprint(r_post.text)
 print(r_post.url)
-
-# with open("skillsoft.html", "wb") as f:
-#     for chunk in r_post.iter_content(chunk_size=10000):
-#         f.write(chunk)
