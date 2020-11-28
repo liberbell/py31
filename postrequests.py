6250f9bf9fbae5c3982d677a8ccddd0335897d0d
Liberbell <liberbell@gmail.com>
2020-11-28 14:05:11 +0900

    postrequests5

--- postrequests.py
+++ postrequests.py
@@ -7,3 +7,7 @@ print(r_post.status_code)
 print(type(r_post))
 
 pprint(r_post.text)
+
+with open("skillsoft.html", "wb") as f:
+    for chunk in r_post.iter_content(chunk_size=10000):
+        f.write(chunk)
