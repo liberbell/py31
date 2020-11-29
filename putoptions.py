2f59c8f7133c7b817847bd9b2d13aaa6114b5c75
Liberbell <liberbell@gmail.com>
2020-11-30 08:54:07 +0900

    putoptions4

--- putoptions.py
+++ putoptions.py
@@ -3,7 +3,10 @@ from pprint import pprint
 import json
 
 URL = "https://httpbin.org/put"
+URL2 = "https://httpbin.org/get"
 r_put = requests.put(URL, data={'key': 'value'})
 
 print(r_put.status_code)
-print(r_put.text)
+# print(r_put.text)
+
+r_option = requests.options(URL2)
