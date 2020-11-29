dfe210d4850637984df4a61fe031f566102f6d85
Liberbell <liberbell@gmail.com>
2020-11-29 23:10:57 +0900

    postmulti3

--- postmulti.py
+++ postmulti.py
@@ -3,4 +3,6 @@ from pprint import pprint
 import webbrowser
 
 post_link = "https://pastebin.com/api/api_post.php"
-payload = "{'username' :'john', 'email' : 'john@example.com'}" 
+payload = "{'username' :'john', 'email' : 'john@example.com'}"
+
+API_key = "068ef8c5332427952bd43af129886ca0"
