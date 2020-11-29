78a8de7583f9ab79dcdfde294c69f9334aa4986d
Liberbell <liberbell@gmail.com>
2020-11-29 23:12:39 +0900

    postmulti4

--- postmulti.py
+++ postmulti.py
@@ -6,3 +6,10 @@ post_link = "https://pastebin.com/api/api_post.php"
 payload = "{'username' :'john', 'email' : 'john@example.com'}"
 
 API_key = "068ef8c5332427952bd43af129886ca0"
+
+parameters = {
+    'api_dev_key': API_key,
+    'api_option': 'paste',
+    'api_paste_code': payload,
+    'api_paste_format': 'python'
+}
