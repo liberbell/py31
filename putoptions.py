6e1c0e70c809abc572011b58d62b0678ff6a04c2
Liberbell <liberbell@gmail.com>
2020-11-30 08:59:05 +0900

    putoptions3

--- putoptions.py
+++ putoptions.py
@@ -4,6 +4,7 @@ import json
 
 URL = "https://httpbin.org/put"
 URL2 = "https://httpbin.org/get"
+URL3 = "https://httpbin.org/delete"
 r_put = requests.put(URL, data={'key': 'value'})
 
 print(r_put.status_code)
@@ -14,3 +15,7 @@ print(type(r_option))
 print(r_option.text)
 
 pprint(r_option.headers)
+
+r_delete = requests.delete(URL3)
+print(r_delete.status_code)
+print(type(r_delete))
