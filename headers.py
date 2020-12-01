86b6897aba4b0802406a40f471cfbde8bbc1dc25
Liberbell <liberbell@gmail.com>
2020-12-01 10:55:15 +0900

    headers8

--- headers.py
+++ headers.py
@@ -6,13 +6,20 @@ url = "https://swapi.dev/api/people/3"
 header1 = {'user-agent' : 'Google Chrome'}
 resp = requests.get(url, headers = header1)
 
-print(resp.status_code)
-print(type(resp))
-print(resp.headers)
-print(resp.headers['content-type'])
+# print(resp.status_code)
+# print(type(resp))
+# print(resp.headers)
+# print(resp.headers['content-type'])
 
 url2 = "https://en.wikipedia.org/wiki/Monty_Python"
-resp_obj = requests.get(url2)
+# resp_obj = requests.get(url2)
 
-print(resp_obj.headers)
-print(resp_obj.request.headers)
+# print(resp_obj.headers)
+# print(resp_obj.request.headers)
+
+url3 = "https://httpbin.org/user-agent"
+header2 = {'user-agent' : 'Internet Explorer/2.0'}
+
+r = requests.get(url3, headers=header2)
+data = r.json()
+print(data)
