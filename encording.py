74ea7a6377dadcbbcaace5f1646d46d4e2ba2cb9
Liberbell <liberbell@gmail.com>
2020-12-02 17:27:32 +0900

    encording4

--- encording.py
+++ encording.py
@@ -4,3 +4,6 @@ from pprint import pprint
 resp_obj = requests.get("https://httpbin.org")
 print(resp_obj.status_code)
 print(resp_obj.encoding)
+
+resp_obj.encoding = "ISO 8859-1"
+print(resp_obj.encoding)
