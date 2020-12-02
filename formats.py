bf7656f10c57b4d6a93fd12ba73b8cfc2a54a04c
Liberbell <liberbell@gmail.com>
2020-12-03 08:44:55 +0900

    formats2

--- formats.py
+++ formats.py
@@ -1,3 +1,8 @@
 import requests
 from pprint import pprint
 import json
+
+url1 = "https://swapi.dev/api/vehicles/4/"
+resp_obj = requests.get(url1)
+print(resp_obj.status_code)
+print(type(resp_obj))
