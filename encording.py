fca0498ff09514b8af527e782de0ac33eb80745c
Liberbell <liberbell@gmail.com>
2020-12-02 17:29:09 +0900

    encording5

--- encording.py
+++ encording.py
@@ -7,3 +7,7 @@ print(resp_obj.encoding)
 
 resp_obj.encoding = "ISO 8859-1"
 print(resp_obj.encoding)
+
+resp_obj = requests.get("https://github.com/timeline.json")
+print(resp_obj.status_code)
+print(resp_obj.encoding)
