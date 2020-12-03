e202b8e6405bf0554d4efc2f76274b2554a6ac20
Liberbell <liberbell@gmail.com>
2020-12-04 08:42:35 +0900

    responsecode6

--- responsecode.py
+++ responsecode.py
@@ -7,10 +7,11 @@ ok_resp = requests.get(url1)
 print(ok_resp.status_code)
 print(ok_resp.ok)
 print(ok_resp.raise_for_status())
+print(ok_resp.headers)
 
 url2 = "https://www.yahoo.com/alf2adfd5"
 bad_resp = requests.get(url2)
 
 print(bad_resp.status_code)
 print(bad_resp.ok)
-print(bad_resp.raise_for_status())
+# print(bad_resp.raise_for_status())
