2e79fc3901c175306d7ec05cd8ba840c9d96d031
Liberbell <liberbell@gmail.com>
2020-12-04 08:38:21 +0900

    responsecode3

--- responsecode.py
+++ responsecode.py
@@ -5,3 +5,10 @@ url1 = "https://example.com"
 ok_resp = requests.get(url1)
 
 print(ok_resp.status_code)
+print(ok_resp.ok)
+
+url2 = "https://www.yahoo.com/alf2adfd5"
+bad_resp = requests.get(url2)
+
+print(bad_resp.status_code)
+print(bad_resp.ok)
