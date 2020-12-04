df504f83a28dba546604581d556a3f83611edd55
Liberbell <liberbell@gmail.com>
2020-12-04 09:01:34 +0900

    redirecttimeout6

--- redirecttimeout.py
+++ redirecttimeout.py
@@ -2,8 +2,9 @@ import requests
 from pprint import pprint
 
 url1 = "https://gmail.com"
+url2 = "https://www.yahoo.co.jp"
 
-resp1 = requests.get(url1)
+resp1 = requests.get(url2)
 print(resp1.status_code)
 print(resp1.history)
 print(resp1.url)
