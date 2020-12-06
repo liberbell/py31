88a9b3fc6b36d12f52bc319e2cee2cdbb25b22bd
Liberbell <liberbell@gmail.com>
2020-12-06 13:05:38 +0900

    exceptionreq5

--- exceptionreq.py
+++ exceptionreq.py
@@ -4,4 +4,9 @@ from requests import exceptions
 
 url1 = "https://nonexistent.com"
 
-resp1 = requests.get(url1)
+# resp1 = requests.get(url1)
+
+try:
+    requests.get(url1)
+except exceptions.ConnectionError:
+    print("Error: Connection error")
