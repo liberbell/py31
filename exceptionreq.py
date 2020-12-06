8cdb14b9cc343e64f03ce02fda7e9c8ca885bab3
Liberbell <liberbell@gmail.com>
2020-12-06 13:07:15 +0900

    exceptionreq6

--- exceptionreq.py
+++ exceptionreq.py
@@ -3,6 +3,7 @@ from pprint import pprint
 from requests import exceptions
 
 url1 = "https://nonexistent.com"
+url2 = "https://github.com"
 
 # resp1 = requests.get(url1)
 
@@ -10,3 +11,5 @@ try:
     requests.get(url1)
 except exceptions.ConnectionError:
     print("Error: Connection error")
+
+resp2 = requests.get(url2, timeout=0.001)
